d7c2f54dc6fa318cd5ab36ca434fca9fb65120a8
guihernandez-ncl <113126799+guihernandez-ncl@users.noreply.github.com>
2023-09-10 22:55:53 -0400

    Add a ctl script and use sshuttle

--- readme.md
+++ readme.md
@@ -1,35 +1,90 @@
-# 
+# SSH Tunneling with Kubernetes
 
-```shell
+This setup allows you to create an SSH tunnel into your internal network using a Kubernetes pod, facilitating access to internal resources without requiring a VPN.
+
+## Prerequisites
+
+Ensure you have the following prerequisites installed and configured:
+
+- `kubectl`
+- `nc` (netcat)
+- `sshuttle`
+- SSH keys configured and added to your SSH agent
+
+On macOS, you can install `nc` and `sshuttle` using Homebrew with the commands:
+
+```sh
+brew install netcat
+brew install sshuttle
+```
+
+Ensure that your KUBECONFIG environment variable is correctly set to point to your Kubernetes cluster.
+
+# Setup
+
+1. Replace the SSH keys in the ssh-pod.yaml file with your own keys.
+2. Set your KUBECONFIG environment variable to point to the correct cluster:
+
+```sh
 export KUBECONFIG=~/.kube/my_cluster_config
 ```
 
-# Create the pod by running:
+3. Add your SSH public keys to your SSH agent (replace with your actual SSH key path):
 
-```shell
-kubectl apply -f ssh-pod.yaml
+```sh
+ssh-add -K ~/.ssh/your-ssh-key
 ```
 
-# Test
+# Using the sshdctl Script
 
-```shell
-kubectl get pods
+We have a shell script named sshdctl that facilitates starting and stopping the SSH tunnel. Below is how you can use it:
+
+## Starting the Tunnel
+
+To start the tunnel with the default network (10.0.0.0/8) and the KUBECONFIG from the environment variable:
+
+Follow the commands below to set up and utilize your SSH tunnel:
+
+```sh
+./sshdctl start
+```
+
+To start the tunnel with a custom network and a custom KUBECONFIG file:
+
+```sh
+./sshdctl start 10.152.0.0/16 /path/to/your/kubeconfig
+```
+
+## Stopping the Tunnel
+
+To stop the tunnel and clean up the Kubernetes resources:
+
+```sh
+./sshdctl stop
+```
+
+## Troubleshooting
+
+If you encounter issues while using the script, ensure the following:
+
+. Your KUBECONFIG environment variable is set correctly.
+. Your SSH keys are correctly added to the SSH agent.
+. The ssh-pod.yaml file is correctly configured with your SSH keys.
+
+For detailed logs, you can check the logs of the SSH Pod using the following command:
+
+```sh
 kubectl logs ssh-pod -c ssh-key-dir
-kubectl describe pod ssh-pod
-kubectl exec -it ssh-pod -- /bin/bash
 ```
 
-# Set up port forwarding
+# Accessing Resources
 
-```shell
-kubectl port-forward ssh-pod 8080:22
+After establishing the SSH tunnel, configure your applications to use the SOCKS proxy at localhost:9090 to direct their traffic through the SSH tunnel. Note that port numbers 9090 and 8080 are just examples and can be changed as long as they are free on your machine.
+
+For a setup that doesn't rely on a SOCKS proxy, consider using a tool like sshuttle.
+
+To SSH into an internal server through the tunnel, use the following command (replace x.x.x.x with the internal IP and myuser with your username):
+
+```sh
+ssh -o ProxyCommand='nc -x localhost:9090 %h %p' myuser@x.x.x.x
 ```
-
-# Establish the SSH tunnel
-
-```shell
-ssh -D 9090 -q -C -N -p 8080 root@localhost
-```
-
-# Delete
-kubectl delete -f ssh-pod.yaml
---
+++ sshdctl.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+set -e
+
+function create_ssh_secret {
+  # List the keys from SSH agent and create a Kubernetes secret from it
+  KEYS=$(ssh-add -L)
+  if [ "$KEYS" == "The agent has no identities." ]; then
+    echo "No keys found in SSH agent. Please add your SSH keys using ssh-add."
+    exit 1
+  else
+    # Creating the Kubernetes secret from your SSH public keys
+    echo "Creating Kubernetes secret from SSH agent keys..."
+    kubectl create secret generic sshdhelper-key-secret --from-literal=ssh-publickey="$KEYS"
+  fi
+}
+
+# Function to check SSH service
+check_ssh_service() {
+  for i in {1..10}; do
+    if kubectl exec ssh-pod -- pgrep sshd > /dev/null; then
+      echo "SSH service is running."
+      return
+    else
+      echo "SSH service is not running, retrying ($i/10)..."
+      kubectl exec ssh-pod -- /usr/sbin/sshd -D &
+      sleep 3
+    fi
+  done
+  echo "Failed to ensure SSH service is running after 10 attempts."
+  exit 1
+}
+
+# Function to check port forwarding
+check_port_forward() {
+  for i in {1..10}; do
+    if ps -o pid,ppid,command -ax | grep "[k]ubectl port-forward"; then
+      echo "Port forwarding is running."
+      return
+    else
+      echo "Port forwarding is not running, retrying ($i/10)..."
+      kubectl port-forward ssh-pod 8080:22 &
+      sleep 5
+    fi
+  done
+  echo "Failed to establish port forwarding after 10 attempts."
+  exit 1
+}
+
+function start_tunnel {
+  local network="${1:-10.0.0.0/8}"
+  local kubeconfig="${KUBECONFIG}"
+
+  if [[ -z "$kubeconfig" ]]; then
+    echo "KUBECONFIG environment variable is not set. Please set it before running the script."
+    exit 1
+  fi
+
+  if [[ -n "$2" ]]; then
+    kubeconfig="$2"
+  fi
+
+  echo "Using KUBECONFIG: $kubeconfig"
+
+  # Set the KUBECONFIG environment variable
+  export KUBECONFIG="$kubeconfig"
+
+  # Call the function to create SSH key secret
+  create_ssh_secret
+
+  # Apply the SSH Pod manifest
+  if ! kubectl apply -f ssh-pod.yaml; then
+    echo "Failed to apply SSH Pod manifest."
+    exit 1
+  fi
+
+  echo "Waiting for the pod to become ready..."
+  while [[ $(kubectl get pods ssh-pod -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do
+    sleep 2;
+  done
+
+  # Check SSH service
+  check_ssh_service
+
+  # Set up port forwarding and check it
+  check_port_forward
+
+  # Start sshuttle
+  if ! sshuttle --dns -vr root@localhost:8080 $network; then
+    echo "Failed to start sshuttle."
+    exit 1
+  fi
+}
+
+function stop_tunnel {
+  set +e
+
+  # Stop sshuttle
+  pkill sshuttle
+
+  # Stop the port-forward
+  pkill -f "kubectl port-forward ssh-pod 8080:22"
+
+  # Delete the SSH Pod
+  if ! kubectl delete -f ssh-pod.yaml; then
+    echo "Failed to delete SSH Pod."
+  fi
+
+  # Delete the secret
+  if ! kubectl delete secret sshdhelper-key-secret; then
+    echo "Failed to delete secret."
+  fi
+
+  echo "Tunnel and resources have been cleaned up"
+}
+
+if [[ "$1" == "start" ]]; then
+  shift
+  start_tunnel "$@"
+elif [[ "$1" == "stop" ]]; then
+  stop_tunnel
+else
+  echo "Usage: $0 {start|stop} [network CIDR] [kubeconfig path]"
+fi
